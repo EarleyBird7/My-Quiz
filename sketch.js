aeac9fe71798c58dfbfb21587050cbb8a060b9bf
EarleyBird7 <72363693+EarleyBird7@users.noreply.github.com>
2021-03-26 17:11:22 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var canva;
+var gameState;
+var contestantCount;
+var database;
+var quiz;
+var question;
+var contestant;
+gameState=0
+
+function setup(){
+  canvas = createCanvas(850,400);
+database=firebase.database()
+}
+
+
+function draw(){
+  background("pink");
+
+  
+}
